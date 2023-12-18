d31b6d7c096b0bdf00cf500f6b450fe912f4b763
Haoru Xue <haorux@andrew.cmu.edu>
2023-12-18 00:26:02 -0500

    simply solution buffer

--- src/tools/lmpc_utils/include/lmpc_utils/mpc_solution_buffer.hpp
+++ src/tools/lmpc_utils/include/lmpc_utils/mpc_solution_buffer.hpp
@@ -53,39 +53,16 @@ public:
  *
  * @param buffer_size the size of the buffer horizon
  */
-  explicit MPCSolutionBuffer(const size_t & buffer_size);
-
-/**
- * @brief Set the current timestamp.
- * The unit of timestamp is 1 MPC cycle.
- * The sliding window of solutions stored will be shifted accordingly.
- * All solutions with timestamp < current_timestamp will be discarded.
- * All solutions with timestamp >= current_timestamp + buffer_size will be discarded.
- * Must be called before calling add_mpc_solution.
- * Must be called at every MPC cycle.
- *
- * @param timestamp
- */
-  void set_current_timestamp(const size_t & timestamp);
-
-/**
- * @brief Get the current timestamp.
- *
- * @return const size_t&
- */
-  const size_t & get_current_timestamp() const;
+  MPCSolutionBuffer();
 
 /**
  * @brief Store a sequence of MPC solutions into the buffer.
- * The solutions will be stored at the starting timestamp.
- * The horizon of the solution does not need to match the buffer size.
- * horizon - 1 inputs and states will be stored.
  *
- * @param x nx by horizon (or nx by horizon - 1), the state trajectory
+ * @param x nx by horizon, the state trajectory
  * @param u nu by horizon - 1, the inputs
  * @param start_timestamp the timestamp of the first state in the trajectory
  */
-  void add_mpc_solution(const casadi::DM & x, const casadi::DM & u, const size_t & start_timestamp);
+  void set_mpc_solution(const casadi::DM & x, const casadi::DM & u, const size_t & start_timestamp);
 
 /**
  * @brief Get the MPC solution at a given timestamp.
@@ -99,13 +76,7 @@ public:
   MPCSolution get_mpc_solution(const size_t & timestamp);
 
 protected:
-  typedef std::vector<MPCSolution> MPCSolutionCircularBuffer;
-
-  MPCSolutionCircularBuffer::iterator get_iterator(const int64_t & delta);
-
-  MPCSolutionCircularBuffer buffer_;
-  MPCSolutionCircularBuffer::iterator current_solution_;
-  size_t current_timestamp_;
+  MPCSolution solution_;
   std::shared_mutex mutex_;
   bool initialized_;
 };
--- src/tools/lmpc_utils/src/mpc_solution_buffer.cpp
+++ src/tools/lmpc_utils/src/mpc_solution_buffer.cpp
@@ -19,74 +19,27 @@ namespace lmpc
 {
 namespace utils
 {
-MPCSolutionBuffer::MPCSolutionBuffer(const size_t & buffer_size)
-: buffer_(buffer_size),
-  current_solution_(buffer_.begin()),
-  current_timestamp_(0),
-  initialized_(false)
+MPCSolutionBuffer::MPCSolutionBuffer()
+: initialized_(false)
 {
-  if (buffer_size == 0) {
-    throw std::invalid_argument("Buffer size cannot be zero.");
-  }
 }
 
-void MPCSolutionBuffer::set_current_timestamp(const size_t & timestamp)
-{
-  std::unique_lock<std::shared_mutex> lock(mutex_);
-  if (!initialized_) {
-    current_timestamp_ = timestamp;
-    initialized_ = true;
-    return;
-  }
-
-  const auto delta = timestamp - current_timestamp_;
-  if (delta > buffer_.size()) {
-    current_timestamp_ = timestamp;
-    return;
-  }
-
-  // Move the current solution iterator
-  current_solution_ = get_iterator(delta);
-  current_timestamp_ = timestamp;
-}
-
-void MPCSolutionBuffer::add_mpc_solution(
+void MPCSolutionBuffer::set_mpc_solution(
   const casadi::DM & x, const casadi::DM & u,
   const size_t & start_timestamp)
 {
   std::unique_lock<std::shared_mutex> lock(mutex_);
   if (!initialized_) {
-    throw std::runtime_error("Must call set_current_timestamp before calling add_mpc_solution.");
+    throw std::runtime_error("Must call set_current_timestamp before calling set_mpc_solution.");
   }
 
-  const auto sol_length = std::min(static_cast<size_t>(u.size2()), buffer_.size());
-
-  // Check if any part of the solution will fall within the buffer
-  if (start_timestamp + sol_length <= current_timestamp_ ||
-    start_timestamp >= current_timestamp_ + buffer_.size())
-  {
-    return;
+  if (x.size2() < u.size2()) {
+    throw std::runtime_error("x must have at least as many columns as u.");
   }
 
-  // Copy the in-range part of solutions into the buffer. Mind the wrap-around.
-  const auto delta = static_cast<int64_t>(start_timestamp) -
-    static_cast<int64_t>(current_timestamp_);
-  const auto start_index = std::max<size_t>(0, -delta);
-  const auto end_index =
-    std::min<size_t>(
-    static_cast<int64_t>(sol_length),
-    static_cast<int64_t>(buffer_.size()) - delta);
-  auto iter = get_iterator(delta + start_index);
-  for (size_t i = start_index; i < end_index; ++i) {
-    iter->x = x(casadi::Slice(), i);
-    iter->u = u(casadi::Slice(), i);
-    iter->status = MPCSolutionStatus::OK;
-    iter->timestamp = start_timestamp + i;
-    ++iter;
-    if (iter == buffer_.end()) {
-      iter = buffer_.begin();
-    }
-  }
+  solution_.x = x;
+  solution_.u = u;
+  solution_.timestamp = start_timestamp;
 }
 
 MPCSolution MPCSolutionBuffer::get_mpc_solution(const size_t & timestamp)
@@ -96,40 +49,20 @@ MPCSolution MPCSolutionBuffer::get_mpc_solution(const size_t & timestamp)
     throw std::runtime_error("Must call set_current_timestamp before calling get_mpc_solution.");
   }
 
-  const auto delta = timestamp - current_timestamp_;
+  const auto delta = timestamp - solution_.timestamp;
+  const auto sol_idx = std::clamp<casadi_int>(delta, 0ul, solution_.u.size2() - 1);
   MPCSolution solution;
-  if (timestamp < current_timestamp_) {
-    solution = *current_solution_;
+  solution.x = solution_.x(casadi::Slice(), sol_idx);
+  solution.u = solution_.u(casadi::Slice(), sol_idx);
+  solution.timestamp = solution_.timestamp + sol_idx;
+  solution.status = MPCSolutionStatus::OK;
+
+  if (timestamp < solution_.timestamp) {
     solution.status = MPCSolutionStatus::TOO_EARLY;
-  } else if (timestamp >= current_timestamp_ + buffer_.size()) {
-    solution = *get_iterator(buffer_.size() - 1);
+  } else if (timestamp >= solution_.timestamp + solution_.u.size2()) {
     solution.status = MPCSolutionStatus::TOO_LATE;
-  } else {
-    solution = *get_iterator(delta);
   }
   return solution;
-}
-
-MPCSolutionBuffer::MPCSolutionCircularBuffer::iterator MPCSolutionBuffer::get_iterator(
-  const int64_t & delta)
-{
-  if (delta == 0) {
-    return current_solution_;
-  } else if (delta > 0) {
-    const auto dist_to_end = std::distance(current_solution_, buffer_.end());
-    if (delta <= dist_to_end) {
-      return current_solution_ + delta;
-    } else {
-      return buffer_.begin() + (delta - dist_to_end) % buffer_.size();
-    }
-  } else {
-    const auto dist_to_begin = std::distance(buffer_.begin(), current_solution_);
-    if (-delta <= dist_to_begin) {
-      return current_solution_ + delta;
-    } else {
-      return buffer_.end() - (-delta - dist_to_begin) % buffer_.size();
-    }
-  }
 }
 }  // namespace utils
 }  // namespace lmpc
